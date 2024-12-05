36260395938002f1b98248377ddb857b4c633c72
Gabriela <gabriela.priaro@gmail.com>
2024-12-05 14:48:27 +0000

    Resoluçao de exercicio pedido ao Chat GPT

---
+++ ExercicioGerenciamentoDeProduto/Produto.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+namespace ExercicioGerenciamentoDeProduto
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public int Codigo { get; set; }
+
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = string.IsNullOrWhiteSpace(value) ? "indefinido" : value;
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set => _preco = value < 0 ? 0 : value;
+        }
+
+        public int Quantidade
+        {
+            get => _quantidade;
+            set => _quantidade = value < 0 ? 0 : value;
+        }
+
+        public Produto(int codigo, string nome, double preco, int quantidade)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        
+        public string ExibirInfomacoes()
+        {
+            return $"Código do produto: {Codigo}\n" +
+                $"Nome: {Nome}\n" +
+                $"Preco: {PrecoToString()}\n" +
+                $"Quantidade: {Quantidade}\n" +
+                $"Valor Total: {ValorTotalToString()}";
+        }
+
+        private double ValorTotal()
+        { 
+            return Preco * Quantidade;
+        }
+
+        private string PrecoToString()
+        { 
+            return Preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        private string ValorTotalToString()
+        {
+            return ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- ExercicioGerenciamentoDeProduto/Program.cs
+++ ExercicioGerenciamentoDeProduto/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace ExercicioGerenciamentoDeProduto
+﻿using System.Globalization;
+
+namespace ExercicioGerenciamentoDeProduto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Codigo do produto: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço do produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(codigo, nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine(produto.ExibirInfomacoes());
+
         }
     }
 }
